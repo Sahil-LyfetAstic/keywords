60635b05b47ad97d0d356f3c8a66ae224629f5fc
sahil-LyfetAstic <sahilsaalu@gmail.com>
2021-11-02 13:16:33 +0530

    sorting completed, errors fixed

--- helpers/sorting.js
+++ helpers/sorting.js
@@ -4,7 +4,7 @@ const db = require("../config/connection");
 
 module.exports = {
   arrSorting: (arr, coll) => {
-    console.log('arr')
+    console.log(arr)
     return new Promise(async (resolve, reject) => {
       await db
         .get()
@@ -20,28 +20,43 @@ module.exports = {
                 ) == index
             );
             resolve(sortedArr);
-          } else {
-            const joinWithoutDupes = (A, B) => {
-              const a = new Set(A.map((x) => x.keyword_name));
-              const b = new Set(B.map((x) => x.keyword_name));
-              return [
-                ...A.filter((x) => !b.has(x.keyword_name)),
-                ...B.filter((x) => !a.has(x.keyword_name)),
-              ];
-            };
-
-            let sortArray = joinWithoutDupes(arr, dbArr);
+          }else{
+            
+            const sortArray = arr.filter(({keyword_name:a}) => !dbArr.some(({keyword_name:b}) => a === b ));
             let result = sortArray.filter(
-              (key, index, array) =>
-                array.findIndex(
-                  (obj) => obj.keyword_name == key.keyword_name
-                ) == index
-            );
-
-            if (result.length === 0) resolve(false);
+                  (key, index, array) =>
+                    array.findIndex(
+                      (obj) => obj.keyword_name == key.keyword_name
+                    ) == index
+                );
+                
+              if (result.length === 0) resolve(false);
             else resolve(result);
-          }
+          } 
         });
     });
-  },
+  }
 };
+
+
+
+// else {
+//   const joinWithoutDupes = (A, B) => {
+//     const a = new Set(A.map((x) => x.keyword_name));
+//     const b = new Set(B.map((x) => x.keyword_name));
+//     return [
+//       ...A.filter((x) => !b.has(x.keyword_name)),
+//       ...B.filter((x) => !a.has(x.keyword_name)),
+//     ];
+//   };
+
+//   let sortArray = joinWithoutDupes(arr, dbArr);
+//   let result = sortArray.filter(
+//     (key, index, array) =>
+//       array.findIndex(
+//         (obj) => obj.keyword_name == key.keyword_name
+//       ) == index
+//   );
+
+
+// }
